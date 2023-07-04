dbf1f45614420e9f514f237fe579c9636d3d2cad
icika-max <icikakennedy@gmail.com>
2023-07-04 11:04:40 +0300

    creating the user and model and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  resources :sessions, only: [:create]
   root to: "static#home"
   # root "articles#index"
 
