f2773bd8f1942a50629ce4f34853563464c9c24e
icika-max <icikakennedy@gmail.com>
2023-07-04 10:24:51 +0300

    setting session and testing the root route

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,12 @@
-Rais.application.config.middleware.insert_before 0, Rack::core do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origin "http://localhost:3000"
-        resource "*", headers: any, methods: [:get, :post, :put, :patch, delete, :options, :head], credentials:true
+      origins "http://localhost:3000"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
+  
     allow do
-        origin "https://service-connect-smmw.vercel.app/"
-        resource "*", headers: any, methods: [:get, :post, :put, :patch, delete, :options, :head], credentials:true
+      origins "https://service-connect-smmw.vercel.app"
+      resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
-end
+  end
+  
